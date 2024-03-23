c129ec21ea444cf690668a836aee306cc8607e6b
Rose <rosebeats09@gmail.com>
2024-03-23 16:26:08 -0400

    Add header section to homepage and shop page routers

--- src/routes/homepage.jsx
+++ src/routes/homepage.jsx
@@ -1,7 +1,8 @@
+import Header from "../components/header"
 export default function Homepage() {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Header />
+        </>
     )
 }
