1d428bfeabd29fa08dd3eecde9ec643dcc4e630b
Rose <rosebeats09@gmail.com>
2024-03-25 14:30:07 -0400

    Fetch all image items and render

--- src/components/best deals section.jsx
+++ src/components/best deals section.jsx
@@ -1,13 +1,22 @@
 import Item from "./item"
-export default function BestDealsSection() {
 
+export default function BestDealsSection() {
+    const itemsArray = [
+        'https://fakestoreapi.com/products/9',
+        'https://fakestoreapi.com/products/10',
+        'https://fakestoreapi.com/products/11',
+        'https://fakestoreapi.com/products/12',
+        'https://fakestoreapi.com/products/13',
+        'https://fakestoreapi.com/products/14'
+    ];
+    
     return (
         <section id="best-deals-section">
             <h4>Today's best deals</h4>
             <div id="items">
-                <Item />
-
-                
+                {itemsArray.map((itemUrl, index) => (
+                    <Item key={index} itemUrl={itemUrl} />
+                ))}
             </div>
         </section>
     )
--- src/components/item.jsx
+++ src/components/item.jsx
@@ -1,32 +1,24 @@
 import { useEffect } from "react";
 import { useState } from "react";
-export default function Item() {
+
+export default function Item({ itemUrl }) {
     const [imageURL, setImageURL] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
 
-    const itemsArray = [
-        'https://fakestoreapi.com/products/9',
-        'https://fakestoreapi.com/products/10',
-        'https://fakestoreapi.com/products/11',
-        'https://fakestoreapi.com/products/12',
-        'https://fakestoreapi.com/products/13',
-        'https://fakestoreapi.com/products/14'
-    ]
-
     useEffect(() => {
-        fetch(itemsArray[0], {mode: "cors"})
-        .then((response) => {
-            if (response.status >= 400) {
-              throw new Error("server error");
-            }
-            return response.json();
-        })
-        .then((response) => setImageURL(response.image))
-        .catch((error) => setError(error))
-        .finally(() => setLoading(false));
+        fetch(itemUrl, { mode: "cors" })
+            .then((response) => {
+                if (response.status >= 400) {
+                    throw new Error("server error");
+                }
+                return response.json();
+            })
+            .then((response) => setImageURL(response.image))
+            .catch((error) => setError(error))
+            .finally(() => setLoading(false));
 
-    }, []);
+    }, [itemUrl]);
 
     if (error) return <p>A network error was encountered</p>;
     if (loading) return <p>Loading...</p>;
