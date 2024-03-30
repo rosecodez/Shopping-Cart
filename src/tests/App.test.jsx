60bbba5ba6e05e608a0b1f917be5d3f2ff628e5a
Rose <rosebeats09@gmail.com>
2024-03-30 19:55:04 -0400

    Wrap test in memory router

--- src/tests/App.test.jsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import Header from '../components/header'
-
-describe('Header component ', () => {
-  it('renders the Header component "Shopping Cart"', () => {
-    render(<Header />)
-    expect(screen.getByRole("heading").textContent).toMatch("Shopping Cart");
-  })
-})
---
+++ src/tests/Header.test.jsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom'; 
+import Header from '../components/header';
+import { describe, it, expect } from 'vitest';
+
+describe('Header component', () => {
+  it('renders the Header component "Shopping Cart and other elements"', () => {
+    render(
+      // wrap header component in a memory router from react router because of the use of link
+      // https://stackoverflow.com/questions/75728532/error-message-uncaught-typeerror-cannot-destructure-property-basename-of-re
+      <MemoryRouter> 
+        <Header />
+      </MemoryRouter>
+    );
+    expect(screen.getByRole('heading').textContent).toMatch("Shopping Cart");
+    screen.getByText("Items:");
+    screen.getByText("All our items");
+    screen.getByText("Today's deals");
+    screen.getByText("Today's deals");
+    screen.getByPlaceholderText(('Search'))
+  });
+});
