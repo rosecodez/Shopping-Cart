55e7a82eeaa98656ee02575cd1dcb6e52cc0e390
Rose <rosebeats09@gmail.com>
2024-04-06 20:14:19 -0400

    Add another section test with beforEach at the same time

--- src/tests/item sections.test.jsx
+++ src/tests/item sections.test.jsx
@@ -1,20 +1,54 @@
 import AllItemsSection from "../components/all items section";
 import BestDealsSection from "../components/best deals section";
 import { render, screen, waitFor } from '@testing-library/react';
-import { describe, it, expect, vi } from "vitest";
+import { describe, it, expect, vi, beforeEach } from "vitest";
 import { MemoryRouter } from "react-router-dom";
 
 const setCartTotal = vi.fn();
 
-render(
-  <MemoryRouter> 
-    <AllItemsSection setCartTotal={setCartTotal} />
-  </MemoryRouter>
-);
-
+// both tests are trying to render components that use the same state variables at the same time,
+// so using beforeEach ensures that each test work with fresh rendered component
 describe('AllItemsSection section', () => {
+  beforeEach(() => {
+    render(
+      <MemoryRouter> 
+        <AllItemsSection setCartTotal={setCartTotal} />
+      </MemoryRouter>
+    );
+  });
+
   it('should render a header "All our products"', () => {
     const header = screen.getByRole('heading', { name: "All our products" });
+    expect(header).toBeInTheDocument();
+  });
+
+  it("every item on section should have a title, a button 'Add to cart', and an image", async () => {
+    await waitFor(() => {
+      const items = document.querySelectorAll('.item');
+      items.forEach(container => {
+        const itemName = container.querySelector('h5');
+        const addButton = container.querySelector('button');
+        const itemImage = container.querySelector('img');
+  
+        expect(itemName).toBeTruthy();
+        expect(addButton).toBeTruthy();
+        expect(itemImage).toBeTruthy();
+      })
+    })
+  })
+});
+
+describe('BestDealsSection section', () => {
+  beforeEach(() => {
+    render(
+      <MemoryRouter> 
+        <BestDealsSection setCartTotal={setCartTotal} />
+      </MemoryRouter>
+    );
+  });
+
+  it('should render a header "Today\'s best deals"', () => {
+    const header = screen.getByRole('heading', { name: "Today's best deals" });
     expect(header).toBeInTheDocument();
   });
   
@@ -33,7 +67,4 @@ describe('AllItemsSection section', () => {
     })
   })
 });
-
-
-
 
